aa43282c0776ea823e7aa2427b4835f83f28a926
HerrMax <maxgreenall@gmail.com>
2017-11-25 22:03:25 +0000

    Updated character controller with VERY TEMPORARY SOUNDS, VERY TEMPORARY

--- PCPolygon/Assets/Scripts/Player/CharController.cs
+++ PCPolygon/Assets/Scripts/Player/CharController.cs
@@ -7,52 +7,120 @@ using UnityEngine;
 
 public class CharController : MonoBehaviour {
 
-    public float speed = 10.0f;
-    public float gravity = 10.0f;
-    public float maxVelocityChange = 10.0f;
-    public bool canJump = true;
-    public float jumpHeight = 2.0f;
-    public bool grounded = false;
+    [SerializeField] private bool isWalking;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] [Range(0f, 1f)] private float runstepLenghten;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float stickToGroundForce;
+    [SerializeField] private float gravityMultiplier;
+    [SerializeField] private AudioClip[] footstepSounds;    
+    [SerializeField] private AudioClip jumpSound;           
+    [SerializeField] private AudioClip landSound;
 
-    public Rigidbody rigid;
+    private CharacterController characterController;
+    private bool jump;
+    private AudioSource audioSource;
+    private bool previouslyGrounded;
+    private bool jumping;
+    private Vector2 input;
+    private Vector3 moveDir = Vector3.zero;
+    private float stepCycle;
+    private float nextStep;
+    private CollisionFlags collisionFlags;
 
-    void Awake () {
-        rigid = this.GetComponent<Rigidbody>();
-        rigid.freezeRotation = true;
-        rigid.useGravity = false;
-	}
-	
-	void FixedUpdate () {
-        if (grounded)
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        audioSource = GetComponent<AudioSource>();
+        stepCycle = 0f;
+        nextStep = stepCycle / 2f;
+        jumping = false;
+    }
+
+    private void Update()
+    {
+        if (!jump)
         {
-            Vector3 targetVeolocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            targetVeolocity = transform.TransformDirection(targetVeolocity);
-            targetVeolocity *= speed;
-
-            Vector3 velocity = rigid.velocity;
-            Vector3 velocityChange = (targetVeolocity - velocity);
-            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-            velocityChange.y = 0;
-            rigid.AddForce(velocityChange, ForceMode.VelocityChange);
-
-            if (canJump && Input.GetKeyDown(KeyCode.Space))
-            {
-                rigid.velocity = new Vector3(velocity.x, CalculateJumpVeritcalSpeed(), velocity.z);
-            }
+            jump = Input.GetButtonDown("Jump");
         }
-        rigid.AddForce(new Vector3(0, -gravity * rigid.mass, 0));
 
-        grounded = false;
+        if (!previouslyGrounded && characterController.isGrounded)
+        {
+            PlayLandingSound();
+            moveDir.y = 0f;
+            jumping = false;
+        }
+        if (!characterController.isGrounded && !jumping && previouslyGrounded)
+        {
+            moveDir.y = 0f;
+        }
+
+        previouslyGrounded = characterController.isGrounded;
     }
 
-    void OnCollisionStay()
+    private void FixedUpdate()
     {
-        grounded = true;
+        float speed;
+        GetInput(out speed);
+    }
+    
+    private void GetInput(out float speed)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        bool wasWalking = isWalking;
+
+        isWalking = !Input.GetKey(KeyCode.LeftShift);
+
+        speed = isWalking ? walkSpeed : runSpeed;
+        input = new Vector2(horizontal, vertical);
+
+        if (input.sqrMagnitude > 1)
+        {
+            input.Normalize();
+        }
     }
 
-    float CalculateJumpVeritcalSpeed()
+    private void PlayJumpSound()
     {
-        return Mathf.Sqrt(2 * jumpHeight * gravity);
+        audioSource.clip = jumpSound;
+        audioSource.Play();
+    }
+
+    private void PlayLandingSound()
+    {
+        audioSource.clip = landSound;
+        audioSource.Play();
+        nextStep = stepCycle + .5f;
+    }
+
+    private void PlayFootStepAudio()
+    {
+        if (!characterController.isGrounded)
+        {
+            return;
+        }
+        int n = Random.Range(1, footstepSounds.Length);
+        audioSource.clip = footstepSounds[n];
+        audioSource.PlayOneShot(audioSource.clip);
+        footstepSounds[n] = footstepSounds[0];
+        footstepSounds[0] = audioSource.clip;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (collisionFlags == CollisionFlags.Below)
+        {
+            return;
+        }
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        body.AddForceAtPosition(characterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
     }
 }
--- PCPolygon/Assets/Scripts/Player/TempPlayerControl.cs
+++ PCPolygon/Assets/Scripts/Player/TempPlayerControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TempPlayerControl : MonoBehaviour {
 
-    public float speed = 6.0F;
+    /*public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
@@ -20,5 +20,176 @@ public class TempPlayerControl : MonoBehaviour {
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }*/
+
+    [SerializeField] private bool isWalking;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] [Range(0f, 1f)] private float runstepLenghten;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float stickToGroundForce;
+    [SerializeField] private float gravityMultiplier;
+    [SerializeField] private AudioClip[] footstepSounds;
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip landSound;
+    [SerializeField] private float stepInterval;
+
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private bool jump;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private bool previouslyGrounded;
+    [SerializeField] private bool jumping;
+    [SerializeField] private Vector2 input;
+    [SerializeField] private Vector3 moveDir = Vector3.zero;
+    [SerializeField] private float stepCycle;
+    [SerializeField] private float nextStep;
+    [SerializeField] private CollisionFlags collisionFlags;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        stepCycle = 0f;
+        nextStep = stepCycle / 2f;
+        jumping = false;
+        audioSource = GetComponent<AudioSource>();
     }
+
+    private void Update()
+    {
+        if (!jump)
+        {
+            jump = Input.GetButtonDown("Jump");
+        }
+
+        if (!previouslyGrounded && characterController.isGrounded)
+        {
+            PlayLandingSound();
+            moveDir.y = 0f;
+            jumping = false;
+        }
+        if (!characterController.isGrounded && !jumping && previouslyGrounded)
+        {
+            moveDir.y = 0f;
+        }
+
+        previouslyGrounded = characterController.isGrounded;
+    }
+
+    private void FixedUpdate()
+    {
+        float speed;
+        GetInput(out speed);
+        print("fixed1");
+        Vector3 desiredMove = transform.forward * input.y + transform.right * input.x;
+
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, characterController.radius, Vector3.down, out hitInfo,
+        characterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+        moveDir.x = desiredMove.x * speed;
+        moveDir.z = desiredMove.z * speed;
+
+        if (characterController.isGrounded)
+        {
+            moveDir.y = -stickToGroundForce;
+
+            if (jump)
+            {
+                moveDir.y = jumpSpeed;
+                PlayJumpSound();
+                jump = false;
+                jumping = true;
+            }
+        }
+        else
+        {
+            moveDir += Physics.gravity * gravityMultiplier * Time.fixedDeltaTime;
+            print("fixed3");
+        }
+        collisionFlags = characterController.Move(moveDir * Time.fixedDeltaTime);
+
+        ProgressStepCycle(speed);
+        print("fixed2");
+    }
+
+    private void GetInput(out float speed)
+    {
+        print("get input");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        bool wasWalking = isWalking;
+
+        isWalking = !Input.GetKey(KeyCode.LeftShift);
+
+        speed = isWalking ? walkSpeed : runSpeed;
+        input = new Vector2(horizontal, vertical);
+
+        if (input.sqrMagnitude > 1)
+        {
+            input.Normalize();
+        }
+    }
+
+    private void ProgressStepCycle(float speed)
+    {
+        if (characterController.velocity.sqrMagnitude > 0 && (input.x != 0 || input.y != 0))
+        {
+            stepCycle += (characterController.velocity.magnitude + (speed * (isWalking ? 1f : runstepLenghten))) *
+                         Time.fixedDeltaTime;
+        }
+
+        if (!(stepCycle > nextStep))
+        {
+            return;
+        }
+
+        nextStep = stepCycle + stepInterval;
+
+        PlayFootStepAudio();
+    }
+
+    private void PlayJumpSound()
+    {
+        audioSource.clip = jumpSound;
+        audioSource.Play();
+    }
+
+    private void PlayLandingSound()
+    {
+        audioSource.clip = landSound;
+        audioSource.Play();
+        nextStep = stepCycle + .5f;
+    }
+
+    private void PlayFootStepAudio()
+    {
+        if (!characterController.isGrounded)
+        {
+            return;
+        }
+        int n = Random.Range(1, footstepSounds.Length);
+        audioSource.clip = footstepSounds[n];
+        audioSource.PlayOneShot(audioSource.clip);
+        footstepSounds[n] = footstepSounds[0];
+        footstepSounds[0] = audioSource.clip;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        print("OnControllerCollider");
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (collisionFlags == CollisionFlags.Below)
+        {
+            return;
+        }
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        body.AddForceAtPosition(characterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
+    }
+
 }
