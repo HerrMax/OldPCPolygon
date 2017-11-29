18a5322d91dc4b65a019649cd86014c826ac8cb2
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2017-11-29 17:54:00 +0000

    BOI
    I fixed the stacking thing...
    Took long enough.

--- PCPolygon Test/Assets/Scripts/InventoryScripts/ItemSlot.cs
+++ PCPolygon Test/Assets/Scripts/InventoryScripts/ItemSlot.cs
@@ -26,13 +26,13 @@ public class ItemSlot : MonoBehaviour, IDropHandler
             item.GetComponent<ItemData>().slot = droppedItem.slot;
             item.transform.SetParent(inv.slots[droppedItem.slot].transform);
             item.transform.position = inv.slots[droppedItem.slot].transform.position;
+                        
+            inv.items[droppedItem.slot] = item.GetComponent<ItemData>().item;
+            inv.items[id] = droppedItem.item;
 
             droppedItem.slot = id;
             droppedItem.transform.SetParent(transform);
             droppedItem.transform.position = transform.position;
-
-            inv.items[droppedItem.slot] = item.GetComponent<ItemData>().item;
-            inv.items[id] = droppedItem.item;
         }
     }
 }
