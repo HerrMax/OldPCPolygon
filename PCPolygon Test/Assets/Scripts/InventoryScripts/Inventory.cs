254b983a121fb2f66e6e24956838d4d77d941d59
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2017-12-02 10:38:24 +0000

    Added Primary and secondary system

--- PCPolygon Test/Assets/Scripts/InventoryScripts/Inventory.cs
+++ PCPolygon Test/Assets/Scripts/InventoryScripts/Inventory.cs
@@ -80,7 +80,7 @@ public class Inventory : MonoBehaviour
         {
             for(int i = 0; i < items.Count; i++)
             {
-                if(items[i].ID == -1)
+                if(items[i].ID == -1 && i >= itemToAdd.Type)
                 {
                     items[i] = itemToAdd;
                     GameObject itemObj = Instantiate(inventoryItem);
--- PCPolygon Test/Assets/Scripts/InventoryScripts/ItemDatabase.cs
+++ PCPolygon Test/Assets/Scripts/InventoryScripts/ItemDatabase.cs
@@ -31,7 +31,7 @@ public class ItemDatabase : MonoBehaviour
     {
         for (int i = 0; i < itemData.Count; i++)
         {
-            database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["stats"]["damage"], (int)itemData[i]["stats"]["range"], (int)itemData[i]["stats"]["rateOfFire"], (int)itemData[i]["stats"]["magSize"], itemData[i]["description"].ToString(), (bool)itemData[i]["stackable"], (int)itemData[i]["rarity"], itemData[i]["slug"].ToString()));
+            database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["stats"]["damage"], (int)itemData[i]["stats"]["range"], (int)itemData[i]["stats"]["rateOfFire"], (int)itemData[i]["stats"]["magSize"], itemData[i]["description"].ToString(), (bool)itemData[i]["stackable"], (int)itemData[i]["rarity"], itemData[i]["slug"].ToString(), (int)itemData[i]["type"]));
         }
     }
 }
@@ -51,9 +51,10 @@ public class Item
     public int Rarity { get; set; }
     public string Slug { get; set; }
     public Sprite Sprite { get; set; }
+    public int Type { get; set; }
 
     //For adding an Item normally
-    public Item(int id, string title, int damage, int range, int rateOfFire, int magsize, string description, bool stackable, int rarity, string slug)
+    public Item(int id, string title, int damage, int range, int rateOfFire, int magsize, string description, bool stackable, int rarity, string slug, int type)
     {
         ID = id;
         Title = title;
@@ -66,6 +67,7 @@ public class Item
         Rarity = rarity;
         Slug = slug;
         Sprite = Resources.Load<Sprite>("InventorySprites/Icons/" + slug);
+        Type = type;
     }
 
     //For adding a null item
