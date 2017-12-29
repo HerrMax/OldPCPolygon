96ec0588c8f980c775d67ac108a7e94ca5d10df8
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2017-12-29 13:36:56 +0000

    Package Added to Main
    All done under a folder named InventoryPackage

---
+++ PCPolygon/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsDisable;
+    [SerializeField]
+    GameObject playerModel;
+
+    Camera mainCamera;
+
+    void Start() {
+        if (!isLocalPlayer) {
+            playerModel.SetActive(true);
+            for (int i = 0; i < componentsDisable.Length; i++) {
+                componentsDisable[i].enabled = false;
+            }
+        }else {
+            mainCamera = Camera.main;
+            if(mainCamera != null) {
+                mainCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable() {
+        if(mainCamera != null) {
+            mainCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ PCPolygon/Assets/Scripts/Sway.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sway : MonoBehaviour {
+
+    public float mouseX;
+    public float mouseY;
+
+    public Quaternion rotationSpeed;
+
+    public float speed;
+
+    void Update () {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotationSpeed = Quaternion.Euler(-mouseY, mouseX, 0);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, rotationSpeed, speed * Time.deltaTime);
+	}
+}
