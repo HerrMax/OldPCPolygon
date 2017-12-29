ea7fb2613688cd8b2111d12740f95f4dd3bdf0de
HerrMax <maxgreenall@gmail.com>
2017-12-29 14:24:53 +0000

    Fixed UI

--- PCPolygon/Assets/Scripts/UIManager.cs
+++ PCPolygon/Assets/Scripts/UIManager.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    [SerializeField] private Behaviour[] disableOnToggle;
+    [SerializeField] private GameObject crosshair;
+
+    [SerializeField] private GameObject inventory;
+    [SerializeField] private KeyCode inventoryKey = KeyCode.Tab;
+
+    void Start()
+    {
+        if(inventory == null)
+        {
+            inventory = GameObject.FindWithTag("InventoryPanel");
+            crosshair = GameObject.FindWithTag("Crosshair");
+            inventory.SetActive(false);
+        }
+    }
+
+    void toggle(GameObject menuToToggle)
+    {
+        inventory.active = !inventory.active;
+        crosshair.active = !inventory.active;
+        for (int i = 0; i < disableOnToggle.Length; i++)
+        {
+            disableOnToggle[i].enabled = !inventory.active;
+        }
+    }
+
 	void Update () {
-		
+        if (Input.GetKeyDown(inventoryKey)) toggle(inventory);
 	}
 }
