77f596c6a68cff51aeba4d2da0df43760dc5024b
HerrMax <maxgreenall@gmail.com>
2018-01-03 12:57:46 +0000

    Jeff

---
+++ PCPolygon/Assets/Scripts/Utilities/Keycodes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//WIP
+public class Keycodes
+{
+    public KeyCode shoot;
+    public KeyCode aim;
+    public KeyCode forwards;
+    public KeyCode backwards;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode inventory;
+    public KeyCode drop;
+
+    void Start()
+    {
+        PlayerPrefs.GetString("shootkey", "Mouse0");
+        PlayerPrefs.GetString("aimkey", "Mouse1");
+        PlayerPrefs.GetString("forwardskey", "W");
+        PlayerPrefs.GetString("backwardskey", "S");
+        PlayerPrefs.GetString("leftkey", "A");
+        PlayerPrefs.GetString("rightkey", "D");
+        PlayerPrefs.GetString("inventorykey", "Tab");
+        PlayerPrefs.GetString("dropkey", "Mouse3");
+    }
+}
