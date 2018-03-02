d57311d4ceb968b43552d7336d173b74928b4216
HerrMax <maxgreenall@gmail.com>
2018-03-02 15:06:24 +0000

    Comm

---
+++ PCPolygon/Assets/Scripts/Utilities/IgnoreCollisions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisions : MonoBehaviour {
+
+    void OnCollisionEnter (Collision collider) {
+		if(collider.gameObject.tag == "player")
+        {
+            //Physics.IgnoreCollision(collider.collider, this.gameObject.collider);
+        }
+	}
+}
