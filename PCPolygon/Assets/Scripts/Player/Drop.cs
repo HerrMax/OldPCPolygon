95685ce55c2fddde5ab2cd684679dc1b49394062
HerrMax <maxgreenall@gmail.com>
2018-03-03 23:03:38 +0000

    Tidied up scripts

--- PCPolygon/Assets/Scripts/Player/Drop.cs
+++ PCPolygon/Assets/Scripts/Player/Drop.cs
@@ -8,21 +8,6 @@ public class Drop : NetworkBehaviour {
     public Transform dropPos;
     public Inventory inventory;
 
-    //public string slugA;
-
-    //This is temporary, it will eventually be replaced with the item slug.
-    public GameObject objToDrop;
-
-
-    /*void Update () {
-        if(!isLocalPlayer) { return; }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            CmdDropItem("augarino");
-        }
-    }*/
-
     public void DropItem(string slug)
     {
         CmdDropItem(slug);
@@ -36,19 +21,4 @@ public class Drop : NetworkBehaviour {
         NetworkServer.Spawn(go);
         Debug.Log("Spawned " + go.transform.name);
     }
-
-    /*[Command]
-    void CmdDropItem(string slug)
-    {
-        RpcDropObj(slug);
-    }
-
-    [ClientRpc]
-    void RpcDropObj(string slug)
-    {
-        GameObject go = (GameObject)Instantiate(Resources.Load(slug), dropPos);
-        go.transform.parent = null;
-        NetworkServer.Spawn(go);
-        Debug.Log("Spawned " + go.transform.name);
-    }*/
 }
