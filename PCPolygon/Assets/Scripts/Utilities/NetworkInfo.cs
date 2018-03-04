782e1ac6e31194b879f771ebd53859742ed8c92b
HerrMax <maxgreenall@gmail.com>
2018-03-04 13:51:14 +0000

    Start on playerc ounter

---
+++ PCPolygon/Assets/Scripts/Utilities/NetworkInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkInfo : NetworkBehaviour {
+
+    public int playerCount;
+    public int ping;
+    public Text playerCountText;
+
+    public override void OnStartClient()
+    {
+        UpdateInfo();    }
+
+    private void OnPlayerConnected(NetworkPlayer player)
+    {
+        UpdateInfo();
+    }
+
+    public void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        UpdateInfo();
+    }
+
+    void UpdateInfo()
+    {
+        playerCount = NetworkManager.singleton.numPlayers;
+        ping = NetworkManager.singleton.client.GetRTT();
+
+        playerCountText.text = "Players : " + playerCount + "/" + NetworkManager.singleton.maxConnections + "\nPing : " + ping + "ms";
+    }
+}
