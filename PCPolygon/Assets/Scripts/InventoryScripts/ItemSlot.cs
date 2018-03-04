7cdca2c286f7eda43698c36360eae280a974fd7f
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2018-03-04 17:39:12 +0000

    FIXED THE BUG

--- PCPolygon/Assets/Scripts/InventoryScripts/ItemSlot.cs
+++ PCPolygon/Assets/Scripts/InventoryScripts/ItemSlot.cs
@@ -20,7 +20,7 @@ public class ItemSlot : MonoBehaviour, IDropHandler
             if(id == 69)
             {
                 inv.drop.DropItem(droppedItem.item.Slug);
-                Destroy(droppedItem.transform.parent);
+                Destroy(droppedItem.gameObject);
             }
             else if (inv.items[id].ID == -1)
             {
